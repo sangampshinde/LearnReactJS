e740852c3b54f5b2916787009decc14cf63015d6
sangam shinde <sangampshinde@gmail.com>
2024-05-22 20:54:16 +0530

    day1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
 function App() {
+  const [status,setStatus]=useState(true);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      {
+        status ? <h1>Hello World</h1> : null
+      }
+      
+      <button onClick={()=>{setStatus(false)}}>Hide</button>
+      <button onClick={()=>{setStatus(true)}}>Show</button>
+      
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React,{useState} from 'react'
+
+//  function App() {
+
+//   const[data,setData]=useState(null);
+//   const[print,setPrint] = useState(false);
+
+//    function getData(val){
+//     console.log(val.target.value);
+//     setData(val.target.value);
+//     setPrint(false)
+//   }
+
+//   return (
+//     <div>
+//         <h1>get input value</h1>
+//         { print ? <p>{data}</p> : null }
+//         <input type='text' onChange={getData}></input>
+//         <button onClick={()=>{setPrint(true)}}>Print Data</button>
+//     </div>
+//   )
+// }
+// export default App;
+
+
+
+
+// import React,{useState} from 'react'
+
+//  function App() {
+//   const[data,setData]=useState(null)
+//    function getData(val){
+//     console.log(val.target.value);
+//     setData(val.target.value);
+
+//   }
+//   return (
+//     <div>
+//         <h1>get input value</h1>
+//         <input type='text' onChange={getData}></input>
+//         <p>{data}</p>
+//     </div>
+//   )
+// }
+// export default App;
