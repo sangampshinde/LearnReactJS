bb91689a86e8bdecd5d8b2023e1328333445c1a1
sangam shinde <sangampshinde@gmail.com>
2024-05-22 22:32:51 +0530

    coditional redaring

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import Profile from './components/Profile';
 
 function App() {
-  const [status,setStatus]=useState(true);
+ 
+
+  
+
   return (
     <div className='App'>
-      {
-        status ? <h1>Hello World</h1> : null
-      }
+      <h1>Conditional Rendaring</h1>
       
-      <button onClick={()=>{setStatus(false)}}>Hide</button>
-      <button onClick={()=>{setStatus(true)}}>Show</button>
-      
+     
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
 
 
 
@@ -23,18 +23,82 @@ export default App
 
 
 
+// import React, { useState } from 'react';
+
+// function App() {
+//   const [name, setName] = useState('');
+//   const [tnc, setTnc] = useState(false);
+//   const [interest, setInterest] = useState('');
+
+//   function getFormData(e) {
+//     e.preventDefault();
+//     console.log(name, tnc, interest);
+//   }
+
+//   return (
+//     <div className='App'>
+//       <h1>Handle form in React</h1>
+//       <form onSubmit={getFormData}>
+//         <input type='text' placeholder='Enter your name' onChange={(e) => setName(e.target.value)}/>
+//         <br /><br />
+//         <select onChange={(e) => setInterest(e.target.value)}>
+//           <option value=''>Select interest</option>
+//           <option value='Marvel'>Marvel</option>
+//           <option value='DC'>DC</option>
+//         </select>
+//         <br /><br />
+//         <input  type='checkbox' onChange={(e) => setTnc(e.target.checked)}/>
+//         <span>Accept terms and conditions</span>
+//         <br /><br />
+//         <button type='submit'>Submit</button>
+//       </form>
+//     </div>
+//   );
+// }
+
+// export default App;
 
 
 
 
+// import React, { useState } from 'react'
+
+// function App() {
+//   const [status,setStatus]=useState(true);
+//   return (
+//     <div className='App'>
+//       {
+//         status ? <h1>Hello World</h1> : null
+//       }
+      
+//       <button onClick={()=>{setStatus(!status)}}>Toggle</button>
+      
+//     </div>
+//   )
+// }
+
+// export default App;
 
 
 
+// import React, { useState } from 'react'
 
+// function App() {
+//   const [status,setStatus]=useState(true);
+//   return (
+//     <div className='App'>
+//       {
+//         status ? <h1>Hello World</h1> : null
+//       }
+      
+//       <button onClick={()=>{setStatus(false)}}>Hide</button>
+//       <button onClick={()=>{setStatus(true)}}>Show</button>
+      
+//     </div>
+//   )
+// }
 
-
-
-
+// export default App;
 
 
 
